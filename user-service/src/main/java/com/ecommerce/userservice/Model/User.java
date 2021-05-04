02afd8eefaa6da576865308b9b21b1655ed3268a
arpanaray77 <arpanaray75897@gmail.com>
2021-05-04 21:23:11 +0530

    Adding Model for user-service

---
+++ user-service/src/main/java/com/ecommerce/userservice/Model/User.java
@@ -0,0 +1,75 @@
+package com.ecommerce.userservice.Model;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="user",uniqueConstraints =  @UniqueConstraint(columnNames="email"))
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	private Collection<Roles> roles;
+	
+	public User(int id, String username, String email, String password, Collection<Roles> roles) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Roles> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Roles> roles) {
+		this.roles = roles;
+	}
+	
+}
