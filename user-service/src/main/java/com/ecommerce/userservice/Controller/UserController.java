bac60b6241c0591bec05460d49036ae4b6d6478d
arpanaray77 <arpanaray75897@gmail.com>
2021-05-06 22:35:11 +0530

    Adding Controller

---
+++ user-service/src/main/java/com/ecommerce/userservice/Controller/UserController.java
@@ -0,0 +1,38 @@
+package com.ecommerce.userservice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.userservice.Controller.DTO.UserRegistrationDto;
+import com.ecommerce.userservice.Service.UserService;
+
+@Controller
+@RequestMapping("/registration")
+public class UserController {
+	
+	private UserService userService;
+
+	public UserController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
+	@PostMapping
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect:/registration?success";
+}
+}
