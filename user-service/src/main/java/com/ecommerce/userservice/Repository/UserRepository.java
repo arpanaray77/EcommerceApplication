ef20f0cab6013d5d6b5fb120e940b20f1143db7c
arpanaray77 <arpanaray75897@gmail.com>
2021-05-06 22:36:11 +0530

    Adding Repository

---
+++ user-service/src/main/java/com/ecommerce/userservice/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.userservice.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.userservice.Model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findByEmail(String email);
+}
