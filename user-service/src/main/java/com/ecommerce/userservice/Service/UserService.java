1f3eb4b0184580116a5b515071bccee04c9f41e5
arpanaray77 <arpanaray75897@gmail.com>
2021-05-07 22:27:25 +0530

    Customzing spring security

--- user-service/src/main/java/com/ecommerce/userservice/Service/UserService.java
+++ user-service/src/main/java/com/ecommerce/userservice/Service/UserService.java
@@ -1,11 +1,14 @@
 package com.ecommerce.userservice.Service;
 
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.ecommerce.userservice.Controller.DTO.UserRegistrationDto;
 import com.ecommerce.userservice.Model.User;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 	
 	User save(UserRegistrationDto registrationdto);
-
 }
--- user-service/src/main/java/com/ecommerce/userservice/Service/UserServiceImpl.java
+++ user-service/src/main/java/com/ecommerce/userservice/Service/UserServiceImpl.java
@@ -1,7 +1,14 @@
 package com.ecommerce.userservice.Service;
 
 import java.util.Arrays;
-
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.userservice.Controller.DTO.UserRegistrationDto;
@@ -12,8 +19,12 @@ import com.ecommerce.userservice.Repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService{
 	
-	//@Autowired
+	
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 
 	public UserServiceImpl(UserRepository userRepository) {
 		super();
@@ -23,10 +34,25 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public User save(UserRegistrationDto registrationDto)
 	{
-		User user =new User(registrationDto.getUsername(),registrationDto.getEmail(),registrationDto.getPassword(),
+		User user =new User(registrationDto.getUsername(),registrationDto.getEmail(),passwordEncoder.encode(registrationDto.getPassword()), 
 				Arrays.asList(new Roles("ROLE_USER")));
 		
 		return userRepository.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		 User user = userRepository.findByEmail(username);
+		 if(user == null)
+		 {
+			 throw new UsernameNotFoundException("Invalid username or password");
+		 }
+		 
+		 return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));		
+	}
+	
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Roles> roles){
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRolename())).collect(Collectors.toList());
 	}
 
 }
