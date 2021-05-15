68d67f5d7c3c02b70320e2f606be817fb8dde86c
arpanaray77 <arpanaray75897@gmail.com>
2021-05-15 16:02:11 +0530

    Refactoring dependencies and other stuffs

--- product-catalog-service/src/main/java/com/ecommerce/productcatalogservice/Controller/ProductController.java
+++ product-catalog-service/src/main/java/com/ecommerce/productcatalogservice/Controller/ProductController.java
@@ -1,17 +1,44 @@
 package com.ecommerce.productcatalogservice.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
-@RequestMapping("/")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.productcatalogservice.Model.Product;
+import com.ecommerce.productcatalogservice.Service.ProductService;
+
+@RestController
 public class ProductController {
 	
-	@GetMapping
-	public String index()
-	{
-		return "index";
-	}
+	@Autowired
+	public ProductService productService;
+	
+	@GetMapping(value = "/getProductList")
+	 public List<Product> getAllProducts(){
+        List<Product> products =  productService.getAllProduct();
+        return products;
+    }
+	
+	@GetMapping(value = "/products", params = "category")
+    public List<Product> getAllProductByCategory(@RequestParam ("category") String category){
+        List<Product> products = productService.getAllProductByCategory(category);
+        return products;
+    }
+
+    @GetMapping (value = "/products/{id}")
+    public Product getOneProductById(@PathVariable ("id") long id){
+        Product product =  productService.getProductById(id);
+       return product;
+    }
+
+    @GetMapping (value = "/products", params = "name")
+    public List<Product> getAllProductsByName(@RequestParam ("name") String name){
+        List<Product> products =  productService.getAllProductByName(name);
+       return products;
+    }
 
 }
