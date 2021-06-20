16f0633b72c895563564fb0fff5bc50977ae4350
arpanaray77 <arpanaray75897@gmail.com>
2021-06-20 14:05:18 +0530

    using RestTemplate for calling Product-catalog Service

--- user-service/src/main/java/com/ecommerce/userservice/Controller/MainController.java
+++ user-service/src/main/java/com/ecommerce/userservice/Controller/MainController.java
@@ -1,19 +1,27 @@
 package com.ecommerce.userservice.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
 
+	RestTemplate restTemplate = new RestTemplate();
+	String indexpg = "http://localhost:8086/shop";
+	ResponseEntity<String> response = restTemplate.getForEntity(indexpg, String.class);
+	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 
 	@GetMapping("/")
-	public String home() {
-		return "index";
+	public ResponseEntity<String> home() {
+		return response;
 	}
-
+    
+	
+	
 }
