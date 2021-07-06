bfb846a68b854427b5ef93e26861c8e3fb124722
arpanaray77 <arpanaray75897@gmail.com>
2021-07-06 17:27:20 +0530

    Adding Model Class for CartItem and Cart with Dto classes

---
+++ cart-service/src/main/java/com/ecommerce/cartservice/model/CartItem.java
@@ -0,0 +1,127 @@
+package com.ecommerce.cartservice.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.ecommerce.cartservice.controller.DTO.CartDto;
+
+@Entity
+@Table(name="cartitem")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_id")
+    private @NotBlank Integer userId;
+
+    @Column(name = "product_id")
+    private @NotBlank Long productId;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Product product;
+
+
+    private Integer quantity;
+
+    public CartItem() {
+    }
+
+
+    public CartItem(CartDto cartDto, Product product,int userId){
+        this.userId = userId;
+        this.productId=cartDto.getProduct().getId();
+        this.quantity=cartDto.getQuantity();
+        this.product = product;
+        this.createdDate = new Date();
+    }
+
+    public CartItem (@NotBlank Integer userId, @NotBlank Long productId, int quantity) {
+        this.userId = userId;
+        this.productId = productId;
+        this.createdDate = new Date();
+        this.quantity = quantity;
+    }
+
+    public CartItem(CartDto cartDto, Product product) {
+        this.productId = cartDto.getProduct().getId();
+        this.quantity = cartDto.getQuantity();
+        this.product = product;
+        this.createdDate = new Date();
+    }
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+
+	public Long getProductId() {
+		return productId;
+	}
+
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+    
+}
